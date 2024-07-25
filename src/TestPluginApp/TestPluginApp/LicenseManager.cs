62a82e0bac298fe9c86cbfb7dce4bde3270b7ccc
vladimir.shvoev <vladimir.shvoev@50ohm.tech>
2024-07-25 17:42:21 +0700

    Добавлены комментарии

--- src/TestPluginApp/TestPluginApp/LicenseManager.cs
+++ src/TestPluginApp/TestPluginApp/LicenseManager.cs
@@ -5,11 +5,20 @@
 /// </summary>
 internal static class LicenseManager
 {
+    /// <summary>
+    /// Проверяет, добавлен ли файл лицензии.
+    /// </summary>
+    /// <returns>True, если есть лицензия. Иначе false.</returns>
     internal static bool CheckLicense()
     {
         return License.Status.Licensed;
     }
 
+    /// <summary>
+    /// Проверяет, есть ли в записи о плагине в файле лицензии.
+    /// </summary>
+    /// <param name="pluginName">Название плагина.</param>
+    /// <returns>True, если есть запись о проверяемом плагине. Иначе false.</returns>
     internal static bool CheckPluginLicense(string pluginName)
     {
         return License.Status.KeyValueList.ContainsKey(pluginName);
